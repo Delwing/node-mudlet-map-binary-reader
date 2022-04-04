cb8d707a915209ae66d25a1d32e1164c8ebed4bf
Piotr Wilczynski <delwing@gmail.com>
2022-04-04 19:14:02 +0200

    raw special exits will be created from mSpecialExits and mSpecialExitLocks

--- map-operations.js
+++ map-operations.js
@@ -3,6 +3,23 @@ const { QUserType } = require("./types/mudlet-models");
 const fs = require("fs");
 
 function writeMap(map, file) {
+
+    for (const roomId in map.rooms) {
+        let rawSpecialExits = {}
+        const room = map.rooms[roomId]
+        for (const exit in room.mSpecialExits) {
+            if (Object.hasOwnProperty.call(room.mSpecialExits, exit)) {
+                const exRoomId = room.mSpecialExits[exit];
+                if (rawSpecialExits[exRoomId] == undefined) {
+                    rawSpecialExits[exRoomId] = []
+                }
+                rawSpecialExits[exRoomId].push((room.mSpecialExitLocks.indexOf(exRoomId) > -1 ? "1" : "0") + exit)
+            }
+        }
+        room.rawSpecialExits = rawSpecialExits
+        
+    }
+
   fs.writeFileSync(file, QUserType.get("MudletMap").from(map).toBuffer(true));
 }
 
@@ -21,7 +38,7 @@ function readMap(file) {
           elements.forEach((ex) => {
             if (ex.startsWith("0")) {
               room.mSpecialExits[ex.substring(1)] = key;
-            } else if (key.startsWith("1")) {
+            } else if (ex.startsWith("1")) {
               room.mSpecialExits[ex.substring(1)] = key;
               room.mSpecialExitLocks.push(ex.substring(1));
             } else {
